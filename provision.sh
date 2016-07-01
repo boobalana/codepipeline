a753952eb9013a2cb1a0f4322e574698fe0aa52f
Boobalan A <boobalana85@gmail.com>
2016-07-01 16:29:47 +0530

    script files

---
+++ provision.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl https://raw.githubusercontent.com/boobalana/bbrepo1/delivery-branch/config -O /home/adminuser/.ssh/config
+curl https://raw.githubusercontent.com/boobalana/bbrepo1/delivery-branch/id_rsa -O /home/adminuser/.ssh/id_rsa
